9a64930c2fcc1d5ff28a95d3d7c57c51262c218a
Shubham <92636412+ShubhamDesai2003@users.noreply.github.com>
2024-06-04 05:49:51 +0530

    issues

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 
 // cd56eaaa
 
-const API_URL = "http://www.omdbapi.com?apikey=cd56eaaa";
+const API_URL = "https://www.omdbapi.com?apikey=cd56eaaa";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
